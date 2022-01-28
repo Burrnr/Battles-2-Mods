0e4d11cd3e12fff33699d816a13d7c82cb4ed25a
Burrnr <98428713+Burrnr@users.noreply.github.com>
2022-01-28 17:22:49 -0600

    Add files via upload

---
+++ New Towers/start.lua
@@ -0,0 +1,6 @@
+mod_metadata("name", "New Towers")
+mod_metadata("author", "Burrnr and 2MuchDiscord")
+mod_metadata("version", "0.1.0")
+
+jet_binary_replace("game_data/game_project/assets/towers/dart_monkey/data/dart_monkey.tower_blueprint", "dart_monkey.tower_blueprint")
+jet_binary_replace("game_data/game_project/assets/towers/bomb_shooter/data/bomb_shooter.tower_blueprint", "bomb_shooter.tower_blueprint")
