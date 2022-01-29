e5d7d62fe68f0306c66ee220482b10b488bd0f42
Burrnr <98428713+Burrnr@users.noreply.github.com>
2022-01-29 12:24:10 -0600

    Add files via upload

---
+++ different_projectile_amounts/start.lua
@@ -0,0 +1,10 @@
+mod_metadata("name", "Different Projectile Amounts")
+mod_metadata("author", "Burrnr")
+mod_metadata("version", "1.0.0")
+
+
+
+jet_binary_replace("game_data/game_project/assets/towers/tack_shooter/data/tack_shooter.tower_blueprint", "tack_shooter.tower_blueprint")
+jet_binary_replace("game_data/game_project/assets/towers/druid/data/druid_blueprint.tower_blueprint", "druid_blueprint.tower_blueprint")
+
+
